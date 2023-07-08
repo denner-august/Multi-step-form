d0ca60b2207886547c78c066768f3b309459c9d3
denner augusto <denner.augusto90@gmail.com>
2023-07-07 21:52:25 -0300

    Update page.tsx

--- step-form/src/app/page.tsx
+++ step-form/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Container } from '@/components/layout/Container'
 import styles from '../styles/page.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <Container />
     </div>
   )
 }
