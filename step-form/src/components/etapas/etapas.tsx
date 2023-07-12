a73e8f05b9f9f3ce6f5132c5789c175b233d8168
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-07-12 15:40:55 -0300

    Update etapas.tsx

--- step-form/src/components/etapas/etapas.tsx
+++ step-form/src/components/etapas/etapas.tsx
@@ -1,11 +1,12 @@
+"use client"
 import { generationNumberSteps } from './function'
 import s from './styles.module.scss'
 export function Etapas() {
     return (
-        <div className={s.Container}>
+        <aside className={s.Container}>
            <ul className={s.Container_list}>
                {generationNumberSteps()}
            </ul>
-        </div>
+        </aside>
     )
 }
