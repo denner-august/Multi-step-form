12e4fd6ef25c457c1a7c91e7ebe8aea8183a38b6
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-07-13 17:52:58 -0300

    component visual  de plano

--- step-form/src/components/form/plano/plano.tsx
+++ step-form/src/components/form/plano/plano.tsx
@@ -1,3 +1,61 @@
-export function Plano(){
- return <h1>plano atual</h1>
+'use client'
+import { useState } from "react";
+import { Switch, Stack } from "@chakra-ui/react";
+import { userStore } from "../../../../context/zustand1";
+
+import s from "./styles.module.scss";
+
+import { ImageComponent } from "@/tools/image";
+
+import {planos} from './planos'
+
+export function Plano() {
+ const { actions:{ProximaEtapa, EtapaAnterior}} = userStore()
+
+  const [state, setState] = useState(1);
+  const [setOn, setOff] = useState(false);
+
+  return (
+    <div className={s.Container}>
+      <div>
+       <h2>Escolha seu plano</h2>
+       <span>você pode escolher pagamento mensal ou anual</span>
+      </div>
+
+      <ul>
+        {planos.map(({ id, nome, imagem, height, width, alt, mensal, anual }) => {
+          return (
+            <li
+              key={id}
+              style={{
+                borderColor: state === id ? " rgb(95, 79, 167)" : undefined,
+              }}
+              onClick={() => setState(id)}
+            >
+              <ImageComponent imagem={imagem} width={width} height={height} alt={alt}/>
+              
+                <div>
+                  {nome}
+                  <span>{setOn === false ? mensal : anual }</span>
+                  <span>{setOn === true ? 'dois meses gratis' : null }</span>
+                </div>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className={s.divCheckBox}>
+        <span style={{color:setOn === false ? 'blue': undefined}}>Mensal</span>
+        <Stack direction="row">
+          <Switch colorScheme="blue" size="lg" onChange={(e)=> setOff(e.target.checked)} />
+        </Stack>
+        <span style={{color:setOn === true ? 'blue': undefined}} >Anual</span>
+      </div>
+
+      <div className={s.Buttons}>
+        <button onClick={EtapaAnterior}>Voltar</button>
+        <button onClick={ProximaEtapa}>Proximo passo</button>
+      </div>
+    </div>
+  );
 }
