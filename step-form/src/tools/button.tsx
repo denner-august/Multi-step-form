e205cbc700ddec308d35f61ca7d1a4466a48e1ee
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-07-15 17:02:23 -0300

    Create button.tsx

---
+++ step-form/src/tools/button.tsx
@@ -0,0 +1,19 @@
+import { userStore } from '../../context/zustand1'
+import s from './styleTools/button.module.scss'
+
+interface buttonProps{
+  NextButton:string | undefined,
+  BackButton:string | undefined,
+  render:Boolean | undefined
+}
+
+export function Button({render=true, BackButton="Voltar", NextButton="Proximo passo"}: buttonProps){
+ const { actions:{ProximaEtapa, EtapaAnterior}} = userStore()
+
+ return (
+  <div className={s.Buttons}>
+  {render === true ?  <button onClick={EtapaAnterior}>{BackButton}</button> : null }
+  <button  onClick={ProximaEtapa}>{NextButton}</button>
+</div>
+ )
+}
