c1cdd552fc7feb3c56a65965b51d5479e33bebc4
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-07-24 13:08:00 -0300

    Update perfil.tsx

--- step-form/src/components/form/seu perfil/perfil.tsx
+++ step-form/src/components/form/seu perfil/perfil.tsx
@@ -1,17 +1,29 @@
 "use client"
-import { useForm } from 'react-hook-form';
 import s from './styles.module.scss';
-import { userStore } from '../../../../context/zustand1';
+import { FieldErrors, useForm } from 'react-hook-form';
 import { Header } from '@/tools/header';
+import { formProps } from '@/types/formProps';
+import { userStore } from '../../../../context/zustand1';
+import {etapaProps, useFormContext} from '../../../../context/Contextform'
+
+import {zodResolver} from '@hookform/resolvers/zod'
+import { ContextValidation } from '@/validation/zod';
 
 export function Perfil() {
     const { actions:{ProximaEtapa}} = userStore();
+    const {actions:{adicionarNome,adicionarEmail,adicionarTelefone}} = useFormContext()
 
-    async function formResposta(data:any, ){
+    async function formResposta({nome,email, telefone}:formProps ){
+        adicionarNome(nome)
+        adicionarEmail(email)
+        adicionarTelefone(telefone)
         ProximaEtapa()
     };
 
-    const {register, handleSubmit,formState:{errors}} = useForm();
+    const {register, handleSubmit,formState:{errors}, setError,  } = useForm<formProps>({
+        resolver:zodResolver(ContextValidation.dados),       
+    });
+    
 
     return (
         <div className={s.Container}>
@@ -21,13 +33,16 @@ export function Perfil() {
             </div>
 
             <form onSubmit={handleSubmit(formResposta)}>
-                <span>Nome</span>
-                <input alt='nome' type="text" placeholder='Digite seu nome' {...register('nome')}/>
+                <span>Nome {errors.nome?.message && `${errors.nome.message}`} </span>
+              
+                <input alt='nome' type="text" placeholder={"Digite seu nome"} {...register('nome')}  />
 
-                <span>Email</span>
+                <span>Email {errors.email?.message && `${errors.email?.message}`}  </span>
+                
                 <input alt='email' type="email" placeholder='Digite seu Email' {...register('email')}  />
 
-                <span>Telefone</span>
+                <span>Telefone {errors.telefone?.message && `${errors.telefone?.message}`}</span>
+
                 <input alt='telefone' type="number" placeholder='Digite seu telefone' {...register('telefone')} />
 
                 <button type="submit">proximo</button>
