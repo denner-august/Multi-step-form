b6403e2f3e1fb4448176fa86a8753ae226999dcd
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-07-26 21:40:14 -0300

    Update button.tsx

--- step-form/src/tools/button.tsx
+++ step-form/src/tools/button.tsx
@@ -1,19 +1,15 @@
-import { userStore } from '../../context/zustand1'
+import { ReactElement, ReactNode } from 'react'
 import s from './styleTools/button.module.scss'
 
 interface buttonProps{
-  NextButton:string | undefined,
-  BackButton:string | undefined,
-  render:Boolean | undefined
+  children:ReactNode
 }
 
-export function Button({render=true, BackButton="Voltar", NextButton="Proximo passo"}: buttonProps){
- const { actions:{ProximaEtapa, EtapaAnterior}} = userStore()
+export function Button({children}: buttonProps){
 
  return (
   <div className={s.Buttons}>
-  {render === true ?  <button onClick={EtapaAnterior}>{BackButton}</button> : null }
-  <button  onClick={ProximaEtapa} type='submit'>{NextButton}</button>
-</div>
+    {children}
+  </div>
  )
 }
