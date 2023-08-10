0b443664f874e5c378d528c431540ff759c68512
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-08-10 16:36:33 -0300

    Update perfil.tsx

--- step-form/src/components/form/seu perfil/perfil.tsx
+++ step-form/src/components/form/seu perfil/perfil.tsx
@@ -1,53 +1,81 @@
-"use client"
-import s from './styles.module.scss';
-import { FieldErrors, useForm } from 'react-hook-form';
-import { Header } from '@/tools/header';
-import { formProps } from '@/types/formProps';
-import { userStore } from '../../../../context/zustand1';
-import {etapaProps, useFormContext} from '../../../../context/Contextform'
+"use client";
+import s from "./styles.module.scss";
+import { useForm } from "react-hook-form";
+import { Header } from "@/tools/header";
+import { formProps } from "@/types/formProps";
+import { userStore } from "../../../../context/zustand1";
+import { useFormContext } from "../../../../context/Contextform";
 
-import {zodResolver} from '@hookform/resolvers/zod'
-import { ContextValidation } from '@/validation/zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ContextValidation } from "@/validation/zod";
 
 export function Perfil() {
-    const { actions:{ProximaEtapa}} = userStore();
-    const {actions:{adicionarNome,adicionarEmail,adicionarTelefone}} = useFormContext()
+  const {
+    actions: { ProximaEtapa },
+  } = userStore();
+  const {
+    actions: { adicionarNome, adicionarEmail, adicionarTelefone },
+  } = useFormContext();
 
-    async function formResposta({nome,email, telefone}:formProps ){
-        adicionarNome(nome)
-        adicionarEmail(email)
-        adicionarTelefone(telefone)
-        ProximaEtapa()
-    };
+  function formResposta({ nome, email, telefone }: formProps) {
+    adicionarNome(nome);
+    adicionarEmail(email);
+    adicionarTelefone(telefone);
+    ProximaEtapa();
+  }
 
-    const {register, handleSubmit,formState:{errors}, setError,  } = useForm<formProps>({
-        resolver:zodResolver(ContextValidation.dados),       
-    });
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formProps>({
+    resolver: zodResolver(ContextValidation.dados),
+  });
 
-    return (
-        <div className={s.Container}>
+  return (
+    <div className={s.Container}>
+      <div className={s.titulo}>
+        <Header
+          titulo="Cadastro pessoal"
+          subtitulo="Informe seu nome, email telefone"
+        />
+      </div>
 
-            <div className={s.titulo}>
-                <Header titulo='Cadastro pessoal' subtitulo='Informe seu nome, email telefone'/>
-            </div>
+      <form onSubmit={handleSubmit(formResposta)}>
+        <span>
+          Nome
+          {errors.nome?.message && `${errors.nome.message}`}
+        </span>
 
-            <form onSubmit={handleSubmit(formResposta)}>
-                <span>Nome {errors.nome?.message && `${errors.nome.message}`} </span>
-              
-                <input alt='nome' type="text" placeholder={"Digite seu nome"} {...register('nome')}  />
+        <input
+          alt="nome"
+          type="text"
+          placeholder={"Digite seu nome"}
+          {...register("nome")}
+        />
 
-                <span>Email {errors.email?.message && `${errors.email?.message}`}  </span>
-                
-                <input alt='email' type="email" placeholder='Digite seu Email' {...register('email')}  />
+        <span>Email {errors.email?.message && `${errors.email?.message}`}</span>
 
-                <span>Telefone {errors.telefone?.message && `${errors.telefone?.message}`}</span>
+        <input
+          alt="email"
+          type="email"
+          placeholder="Digite seu Email"
+          {...register("email")}
+        />
 
-                <input alt='telefone' type="number" placeholder='Digite seu telefone' {...register('telefone')} />
+        <span>
+          Telefone {errors.telefone?.message && `${errors.telefone?.message}`}
+        </span>
 
-                <button type="submit">proximo</button>
-            </form>
-        </div>
-    )
+        <input
+          alt="telefone"
+          type="number"
+          placeholder="Digite seu telefone"
+          {...register("telefone")}
+        />
+
+        <button type="submit">proximo</button>
+      </form>
+    </div>
+  );
 }
-
